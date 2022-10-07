eeb2158006f26a87c1b396bea24863385c0621df
jiangwenbin <wenbin.jiang@coollu.com.cn>
2022-10-07 23:56:19 +0800

    chore: 临时提交

---
+++ packages/vue-pro-components/src/icon/icon.vue
@@ -0,0 +1,14 @@
+<template>
+    <i>{{icon}}</i>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: '默认图标'
+        }
+    }
+}
+</script>
