79c4899ae6c5ae5aaacff2242e22367394f02333
Tusi <cumtrobin@163.com>
2022-10-25 00:09:44 +0800

    chore: 临时提交，调试eslint+ts效果

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,29 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ['standard-with-typescript', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-essential', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:vue/vue3-essential',
+        'prettier',
+    ],
     plugins: ['@typescript-eslint', 'vue', 'prettier'],
-    parser: '@typescript-eslint/parser',
+    parser: 'vue-eslint-parser',
     parserOptions: {
+        "parser": {
+            "js": "espree",
+            "ts": "@typescript-eslint/parser",
+            "<template>": "espree"
+        },
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: {
             jsx: true,
         },
         tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
+        project: ['./tsconfig.eslint.json'],
+        extraFileExtensions: ['.vue'],
     },
     rules: {
         indent: ['warn', 4],
--- packages/vue-pro-components/src/icon/icon.vue
+++ packages/vue-pro-components/src/icon/icon.vue
@@ -2,7 +2,7 @@
     <i>{{ icon }} </i>
 </template>
 
-<script>
+<script lang="ts">
 export default {
     props: {
         icon: {
