44092f157071ef8de0d9025c6eb059d811ed3b9b
jiangwenbin <wenbin.jiang@coollu.com.cn>
2022-11-08 19:50:31 +0800

    feat: icon-svg临时代码

---
+++ packages/vue-pro-components/src/icon-svg/icon-svg.vue
@@ -0,0 +1,9 @@
+<template>
+    <i class="iconfont" :style="{ fontSize: `${size}px`, color: color }" :class="`${iconPrefix}${icon}`"></i>
+</template>
+
+<script lang="ts" setup>
+import { props } from './props'
+
+defineProps(props)
+</script>
