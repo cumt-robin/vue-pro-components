cbf8d0f0ec34329a96265238e3315faf27dc1a04
jiangwenbin <wenbin.jiang@coollu.com.cn>
2022-11-16 16:15:54 +0800

    perf: 改进

--- packages/playground/scripts/genIconList.mjs
+++ packages/playground/scripts/genIconList.mjs
@@ -2,20 +2,16 @@
 import fs from "fs"
 import axios from "axios"
 
-const SVG_ICON_SCRIPT_URL = "https://at.alicdn.com/t/c/font_3736402_d50r1yq40hw.js"
-const SVG_ICON_PREFIX = "vp-icon-"
-
-function getIcons(str) {
-    const reg = new RegExp(`id="${SVG_ICON_PREFIX}([^"]+)"`);
-    return str.match(/id="([^"]*)"/g).map((item) => item.replace(reg, "$1"));
-}
+// 致敬喷子，自我改进
+const SVG_ICON_SCRIPT_URL = "https://at.alicdn.com/t/c/font_3736402_d50r1yq40hw.json"
 
 export async function genIconListJson() {
     try {
         const res = await axios.get(SVG_ICON_SCRIPT_URL);
-        console.log(res)
+        
         if (res.status === 200) {
-            const iconList = getIcons(res.data);
+            console.log()
+            const iconList = res.data.glyphs.map(item => item.name)
             console.log(iconList);
             fs.writeFile(new URL("../src/assets/json/icons.json", import.meta.url), JSON.stringify(iconList, null, 2), function (err) {
                 if (err) {
