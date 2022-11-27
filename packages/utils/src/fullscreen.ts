27e6329a1307e0e63e52b8c8175a32e2535a167b
Tusi <cumtrobin@163.com>
2022-11-27 14:27:27 +0800

    perf: 全屏基础api封装完善

--- packages/utils/src/fullscreen.ts
+++ packages/utils/src/fullscreen.ts
@@ -23,7 +23,8 @@ interface EnhancedDocument extends Document {
 /**
  * 进入全屏
  * https://developer.mozilla.org/zh-CN/docs/Web/API/Element/requestFullScreen
- * @param element 全屏目标元素，默认是 body
+ * @param {EnhancedHTMLElement} [element=document.body] - 全屏目标元素，默认是 body
+ * @param {FullscreenOptions} options - 全屏选项
  */
 export async function enterFullscreen(element: EnhancedHTMLElement = document.body, options?: FullscreenOptions) {
     try {
@@ -98,23 +99,48 @@ export function isFullscreen(): boolean {
 /**
  * @description 监听全屏变化事件
  * @param {Function} callback 回调函数
+ * @param {boolean|AddEventListenerOptions} options 监听事件选项
  */
-export function listenFullscreen(callback: () => unknown): void {
+export function listenFullscreen(callback: EventListener, options?: boolean | AddEventListenerOptions): void {
     const doc: EnhancedDocument = document
     if (typeof doc.exitFullscreen !== 'undefined') {
-        doc.addEventListener('fullscreenchange', callback, false)
+        doc.addEventListener('fullscreenchange', callback, options)
     } else if (doc.webkitExitFullscreen) {
-        doc.addEventListener('webkitfullscreenchange', callback, false)
+        doc.addEventListener('webkitfullscreenchange', callback, options)
     } else if (doc.mozCancelFullScreen) {
-        doc.addEventListener('mozfullscreenchange', callback, false)
+        doc.addEventListener('mozfullscreenchange', callback, options)
     } else if (doc.msExitFullscreen) {
-        doc.addEventListener('MSFullscreenChange', callback, false)
+        doc.addEventListener('MSFullscreenChange', callback, options)
     } else {
         throw new Error('该浏览器不支持全屏API')
     }
 }
 
-export function preventF11DefaultAction(): void {
+/**
+ * @description 移除监听全屏变化事件
+ * @param {Function} callback 回调函数
+ * @param {boolean|EventListenerOptions} options 监听事件选项
+ */
+export function unlistenFullscreen(callback: EventListener, options?: boolean | EventListenerOptions): void {
+    const doc: EnhancedDocument = document
+    if (typeof doc.exitFullscreen !== 'undefined') {
+        doc.removeEventListener('fullscreenchange', callback, options)
+    } else if (doc.webkitExitFullscreen) {
+        doc.removeEventListener('webkitfullscreenchange', callback, options)
+    } else if (doc.mozCancelFullScreen) {
+        doc.removeEventListener('mozfullscreenchange', callback, options)
+    } else if (doc.msExitFullscreen) {
+        doc.removeEventListener('MSFullscreenChange', callback, options)
+    } else {
+        throw new Error('该浏览器不支持全屏API')
+    }
+}
+
+/**
+ * 阻止F11按键的默认行为，并根据当前的全屏状态调用进入/退出全屏，
+ * 解决通过F11按键和API两种方式进入全屏时出现的状态不一致问题。
+ */
+export function patchF11DefaultAction(): void {
     window.addEventListener('keydown', (e) => {
         // https://w3c.github.io/uievents-code/
         if (e.code === 'F11') {
