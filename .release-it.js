24a8c2660b78f59506ac1888358303c7c0598a4b
Tusi <cumtrobin@163.com>
2023-01-05 22:01:49 +0800

    chore: 修改钩子

--- .release-it.js
+++ .release-it.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     hooks: {
         // 更新子包版本号
-        "before:git:bump": "yarn packages-bump-version",
+        "before:version:bump": "yarn packages-bump-version",
         'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
     },
 }
