2274ec2e0179a6118f4f784b141168644c97e356
Tusi <cumtrobin@163.com>
2023-01-05 21:39:42 +0800

    chore: 增加 release-it

---
+++ .release-it.js
@@ -0,0 +1,30 @@
+module.exports = {
+    git: {
+        // 具体参考源码 https://github.com/release-it/release-it/blob/master/lib/plugin/git/Git.js
+        addUntrackedFiles: true,
+        commitMessage: 'chore: release v${version}',
+    },
+    // 默认需要 GITHUB_TOKEN 环境变量，可以通过 tokenRef 定制
+    github: {
+        release: true,
+        // 由于我们之前已经配置过 GH_TOKEN，就继续用 GH_TOKEN 吧
+        tokenRef: 'GH_TOKEN',
+    },
+    plugins: {
+        '@release-it/conventional-changelog': {
+            preset: 'angular',
+            infile: 'CHANGELOG.md',
+        },
+    },
+    npm: {
+        // 不做 npm publish 操作，交给 ci/cd 执行
+        publish: false,
+        // ignore package.json version，以 git tag 为准
+        ignoreVersion: true,
+    },
+    hooks: {
+        // 更新子包版本号
+        "before:git:bump": "yarn packages-bump-version",
+        'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
+    },
+}
